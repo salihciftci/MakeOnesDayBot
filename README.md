e31df035e8d843238e127c327c9418f55475fc7c
Salih Çiftçi <salih@ciftci.co>
2017-12-30 19:20:23 +0300

    Changed project structure and improved code

--- README.md
+++ README.md
@@ -1,23 +1,9 @@
-# MakeOnesDayBot
+# Tina [![Build Status](https://travis-ci.org/salihciftci/Tina.svg?branch=master)](https://travis-ci.org/salihciftci/Tina)
 
-Make One's Day Bot is a Twitter bot who mention to people about their life.
+Tina is a Twitter bot who mentions people about their life based on their tweets.
 
-Bot randomly send mention tweet to random people like "Hello", "You're beatiful." etc.
+Tina randomly sends mention tweets to random people like "Hello" or something related with their mood that can be examined by Tina.
 
-Bot's goal is to make people smile. It's still in under development and It's alive right now. [Check it out!](https://twitter.com/MakeOnesDayy)
+Basically, Tina's goal is to make people smile. Tina's development activities still going on and it's already alive and accessible.
 
-# To-do
-
-- [ ] Better search agorithm.
-- [ ] Direct Message.
-- [ ] Response to mentions.
-- [ ] Language support.
-- [ ] Better sentences.
-
-# Contributing
-
-1. Fork the repo on GitHub
-2. Clone the project to your own machine
-3. Commit changes to your own branch
-4. Push your work back up to your fork
-5. Submit a Pull request so that I can review your changes
+twitter.com/TheTinaAI
---
+++ build/tina.py
@@ -0,0 +1,98 @@
+import requests, sys, json, time, random
+from requests_oauthlib import OAuth1
+from urllib.parse import quote_plus as encode
+
+def findPeople():
+	with open("db.json") as file:
+		db = json.load(file)
+
+	# Selecting random keyword
+	keywords = db["keywords"]
+	keyword = keywords[random.randint(0,len(keywords))]
+	print("%s - Tina: Searching people keyword is %s" % (time.ctime(), keyword))
+
+	# Searching people
+	searchUrl = "https://api.twitter.com/1.1/search/tweets.json?q=%s&vertical=news" % keyword
+	r = requests.get(searchUrl, auth=auth)
+	
+	if r.status_code == 200:
+		randomPersonJson = json.loads(r.text)
+
+	global randomPerson
+	randomPerson = []
+	while len(randomPerson) < 15:
+		randomPerson.append(randomPersonJson["statuses"][len(randomPerson)]["user"]["screen_name"])
+
+
+mentionCount = 0
+def tweet(status):
+	tweetUrl = "https://api.twitter.com/1.1/statuses/update.json?status=%s"
+
+	if status == "tweet":
+		randomTweet = tweets[random.randint(0,len(tweets)-1)]
+		r = requests.post(tweetUrl % encode(randomTweet), auth=auth)
+		
+		if r.status_code == 200:
+			print("%s - Tina: tweeted." % time.ctime())
+		elif r.status_code == 403:
+			print("%s - Tina: Tweet duplicated." % time.ctime())
+		else:
+			print("%s - Tina: Unexpected Error, Error Code: %s" % (time.ctime(), r.status_code))
+
+	elif status == "mention":
+		global mentionCount
+		randomMentionTweet = mentionTweets[random.randint(0,len(mentionTweets)-1)]
+		randomPersonToTweet = randomPerson[mentionCount]
+
+		r = requests.post(tweetUrl % "%40" + randomPersonToTweet + "%20" + encode(randomMentionTweet), auth=auth)
+		if r.status_code == 200:
+			print("%s - Tina: Mentioned to %s" % (time.ctime(), randomPersonToTweet))
+		elif r.status_code == 403:
+			print("%s - Tina: Mention tweet duplicated." % time.ctime())
+		else:
+			print("%s - Tina: Unexpected Error, Error Code: %s" % (time.ctime(), r.status_code))
+
+		if mentionCount == 14:
+			findPeople()
+			mentionCount = 0
+		else:
+			mentionCount += 1
+
+
+if __name__ == "__main__":
+	with open("auth.json") as file:
+		authJson = json.load(file)
+
+	authUrl = "https://api.twitter.com/1.1/account/verify_credentials.json"
+	auth = OAuth1(authJson["API_KEY"],authJson["API_SECRET"],authJson["OAUTH_TOKEN"],authJson["OAUTH_TOKEN_SECRET"])
+
+	#Trying to Auth to Twitter
+	r = requests.get(authUrl, auth=auth)
+	if not r.status_code == 200:
+		print("%s - Tina: Auth unsuccessful!" % time.ctime())
+		sys.exit()
+	else:
+		print("%s - Tina: Auth successful!" % time.ctime())
+
+	#Reading db.json for tweets
+	with open("db.json") as file:
+		tweetsJson = json.load(file)
+
+	tweets = tweetsJson["tweets"]
+	mentionTweets = tweetsJson["mentionTweets"]
+
+	#Finding People
+	findPeople()
+	
+	while True:
+		tweet("tweet")
+		time.sleep(901)
+		for x in range (0,16):
+			tweet("mention")
+			time.sleep(901)
+
+
+
+
+
+
